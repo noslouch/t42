db392da611a347547ffe759445105110d88ff815
Brian Whitton <me@brianwhitton.com>
2015-09-12 17:56:44 -0400

    splits up logic, updates to request-promise

--- index.js
+++ index.js
@@ -1,50 +1,59 @@
-var request = require('request'),
-    cheerio = require('cheerio'),
+var request = require('request-promise'),
+    url = require('url'),
     CartoDB = require('cartodb'),
     secrets = require('./secrets.js'),
+    googleJSON = require('./lib/google'),
+    Scraper = require('./lib/newyorker'),
     baseUrl = 'http://www.newyorker.com/magazine/tables-for-two',
     cartodbClient = new CartoDB({user: secrets.USER, api_key: secrets.API_KEY}),
     requestUrl
 
-cartodbClient.on('connect', function() {
-    for (var i = 1; i < 10; i++) {
-        if (i === 1) {
-            requestUrl = baseUrl.slice()
-        } else {
-            requestUrl = baseUrl.slice() + '/page/' + i
-        }
+function geoCb(b, ops) {
+  console.log(body.results)
+    var body = JSON.parse(b),
+        results = body.results[0],
+        coords = results ? results[0].geometry.location : '',
+        coordsString = coords ? coords.lng + ',' + coords.lat : ''
 
-        request(requestUrl, function(error, response, body) {
-            if (error) {
-                console.log('That\'s an error:', error)
-                return
-            }
+    this.dataSet.push({
+        title: ops.title,
+        url: ops.href,
+        address: ops.address,
+        coords: coordsString
+    })
+}
 
-            var $ = cheerio.load(body),
-                articles = $('.posts').find('article')
+function sendToCarto() {
+  var data = scraper.dataSet
+    console.log(data)
 
-            articles.each(function(i, el) {
-                var title = $(el).find('[itemprop=headline]').text(),
-                    href = $(el).find('[itemprop=name]').attr('href'),
-                    address
-
-                request(href, function(error, response, body) {
-                    if (error) {
-                        console.log('error getting post:', error)
-                        return
-                    }
-                    var $ = cheerio.load(body),
-                        address = $('#page-header').find('[itemprop=alternativeHeadline]').text().split(';')
-
-                    // cartodbClient.query("insert into t42 (address, state) values('{address}', 'NY')", {address: address[0]}, function(err, data) {
-                    //     if (err) { console.log(err) } else { console.log(data) }
-                    // })
-                    cartodbClient.query("update t42 set name = '{title}', url = '{href}' where address = '{address}'", {title: title, href: href, address: address[0]}, function(err, data) {
-                         if (err) { console.log(err) } else { console.log(data) }
-                    })
-                })
-            })
-        })
-    }
+  cartodbClient.on('connect', function() {
+    data.forEach(function(d) {
+      cartodbClient.query("insert into t42 (address, state) values('{address}', 'NY')", {address: address[0]}, function(err, data) {
+          if (err) { console.log(err) } else { console.log(data) }
+      })
+      cartodbClient.query("update t42 set name = '{title}', url = '{href}' where address = '{address}'", {title: title, href: href, address: address[0]}, function(err, data) {
+         //if (err) { console.log(err) } else { console.log(data) }
+      })
+    })
 })
-cartodbClient.connect()
+//cartodbClient.connect()
+}
+
+var scraper = new Scraper({
+    getLatLng: googleJSON,
+    geoCb: geoCb
+})
+
+for (var i = 1; i <= 2; i++) {
+    if (i === 1) {
+        requestUrl = baseUrl.slice()
+    } else {
+        requestUrl = baseUrl.slice() + '/page/' + i
+    }
+
+    request(requestUrl).promise().bind(scraper)
+      .then(scraper.scrape)
+      .finally(sendToCarto)
+
+}
